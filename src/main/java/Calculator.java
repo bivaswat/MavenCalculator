3da05f9278415cd29d2dc5a4f0b7d7e74a4cd2f3
philipp012 <philipp.kiss@bluewin.ch>
2019-01-20 12:22:25 +0100

    Added Unittests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,13 +2,17 @@ class Calculator {
     int summe(int value1, int value2) {
         return value1 + value2;
     }
+
     int subtraktion(int value1, int value2) {
         return value1 - value2;
     }
+
     int multiplication(int value1, int value2) {
         return value1 * value2;
     }
+
     int division(int value1, int value2) {
         return value1 / value2;
+
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,29 +1,38 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     private Calculator calculator;
+    private Object ArithmeticException;
+
+    @Before
+    public void initializeCalculator() {
+        calculator = new Calculator();
+    }
     @Test
     public void testSummeZweiPositiveIsOk() {
-        calculator = new Calculator();
         assertEquals(calculator.summe(10, 25), 35);
     }
 
     @Test
     public void testSubtractionZweiPositiveIsOk() {
-        calculator = new Calculator();
         assertEquals(calculator.subtraktion(25, 10), 15);
     }
     @Test
     public void testMultiplicationZweiPositiveIsOk() {
-        calculator = new Calculator();
         assertEquals(calculator.multiplication(10, 10), 100);
     }
 
     @Test
     public void testDivisionZweiPositiveIsOk() {
-        calculator = new Calculator();
         assertEquals(calculator.division(4, 2), 2);
     }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionMitNull() {
+        assertEquals(calculator.division(10, 0), 0);
+    }
+
 }
