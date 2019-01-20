a50b22d6128ce8e7ab4ffd9d7f33128f2c36425b
philipp012 <philipp.kiss@bluewin.ch>
2019-01-20 15:50:53 +0100

    Added Overflow handling

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,18 @@
 class Calculator {
+
     int summe(int value1, int value2) {
-        return value1 + value2;
+        long tester = (long) value1 + (long) value2;
+        overflowhandler(tester);
+        return (int) tester;
+    }
+
+    private void overflowhandler(long tester) {
+        if (tester > Integer.MAX_VALUE) {
+            throw new ArithmeticException("Result is too big");
+        }
+        if (tester < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Result is too small");
+        }
     }
 
     int subtraktion(int value1, int value2) {
