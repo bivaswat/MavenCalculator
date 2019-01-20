2742a717f742c51b45068efe8aa900b944c0e02e
philipp012 <philipp.kiss@bluewin.ch>
2019-01-20 12:24:31 +0100

    Completed App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,9 @@ public class App {
         int valueB = 20;
 
         System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
+        System.out.println("Subtraktion " + valueA + " - " + valueB + " = " + calculator.subtraktion(valueA, valueB));
+        System.out.println("Multiplikation " + valueA + " * " + valueB + " = " + calculator.multiplication(valueA, valueB));
+        System.out.println("Division " + valueA + " / " + valueB + " = " + calculator.division(valueA, valueB));
 
 
     }
